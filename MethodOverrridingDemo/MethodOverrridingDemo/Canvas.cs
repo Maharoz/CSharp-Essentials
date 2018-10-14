4ddc529f371ceb654c32ccfb3bfc6a7afca6113a
Maharoz <mahruj66@gmail.com>
2018-10-15 00:38:30 +0600

    method ovveride implemented

---
+++ MethodOverrridingDemo/MethodOverrridingDemo/Canvas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverrridingDemo
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach(var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
