6c42cc49b6c8c9e8407ca46be2310a272dc4e64d
Maharoz <mahruj66@gmail.com>
2018-10-15 21:35:04 +0600

    public , private and protected demonstration

---
+++ AccessModifiersDemo/AccessModifiersDemo/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to level 1");
+            }
+            else
+            {
+                Console.WriteLine("Promoted to level 2");
+            }
+        }
+        protected int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
