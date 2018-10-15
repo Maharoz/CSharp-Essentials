1b6b0c95deee0e1f796c61f38bd61fd3c5b7a215
Maharoz <mahruj66@gmail.com>
2018-10-15 22:48:06 +0600

    constructor and inheritance demonstrated

---
+++ Constructors/Constructors/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber):base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized.{0}",registrationNumber);
+
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("xyz123");
+        }
+    }
+}
