637ba6217753c7b224defa61270094c75aa62e58
Maharoz <mahruj66@gmail.com>
2018-10-15 23:57:16 +0600

    constructor partially  demonstrated

---
+++ CSharpConstructor/CSharpConstructor/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConstructor
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public Customer()
+        {
+
+        }
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+        public Customer(int id,string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ CSharpConstructor/CSharpConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            //if you debugg following lines will execute first as 
+            //default constructor hits
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
