43129f29d04e9ad5492a4d16aed21d67140db276
Maharoz <mahruj66@gmail.com>
2018-10-15 23:36:38 +0600

    static keyword demonstrated

--- ClassesDemo/ClassesDemo/Program.cs
+++ ClassesDemo/ClassesDemo/Program.cs
@@ -14,17 +14,21 @@ namespace ClassesDemo
             Console.WriteLine("Hi {0} , I am {1}",to,Name);
         }
 
-        public Person Parse(string str)
+        public static Person Parse(string str)
         {
+            var person = new Person();
+            person.Name = str;
 
+            return person;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();//creating the object
-            person.Name = "Maharoz";//adding value to Name property
+            //var person = new Person();//creating the object
+            //person.Name = "Maharoz";//adding value to Name property
+            var person = Person.Parse("mosh");
             person.Introduce("Mosh");//calling introduce method
         }
     }
