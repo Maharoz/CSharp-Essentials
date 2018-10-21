629312f8c53ed299fabf89a3d836b21d8f2d93be
Maharoz <mahruj66@gmail.com>
2018-10-22 00:26:26 +0600

    fields demonstrated

---
+++ CSharpFieldsDemo/CSharpFieldsDemo/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFieldsDemo
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            //this is prohibited because read only fields are only 
+            //on constructor or when it declaring
+            //Orders = new List<Order>();
+        }
+    }
+}
---
+++ CSharpFieldsDemo/CSharpFieldsDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFieldsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+    }
+}
