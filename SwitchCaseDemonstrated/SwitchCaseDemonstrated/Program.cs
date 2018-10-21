ee187143c8c5dc3f6be132c27b9e32859f3c543a
Maharoz <mahruj66@gmail.com>
2018-10-21 22:02:09 +0600

    switch case demonstrated

---
+++ SwitchCaseDemonstrated/SwitchCaseDemonstrated/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCaseDemonstrated
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.autumn;
+            switch (season)
+            {
+                case Season.autumn:
+                    Console.WriteLine("Its autumn an beautifull season");
+                    break;
+                case Season.summer:
+                    Console.WriteLine("Its summer and it is perfect time to go to beach");
+                    break;
+                default:
+                    Console.WriteLine("I dont understand that season");
+                    break;
+            }
+        }
+    }
+}
---
+++ SwitchCaseDemonstrated/SwitchCaseDemonstrated/Season.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCaseDemonstrated
+{
+    public enum Season
+    {
+        summer,
+        spring,
+        autumn,
+        winter
+    }
+}
