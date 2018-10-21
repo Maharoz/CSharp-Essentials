a017f34edc43e58c2fedeabc0c84ef30c43443d1
Maharoz <mahruj66@gmail.com>
2018-10-21 20:54:28 +0600

    List demonstrated

---
+++ CSharpListDemo/CSharpListDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine();
+            Console.WriteLine("LastIndex of 1:" + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+            //numbers.Remove(1);
+            for(var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
+        }
+    }
+}
