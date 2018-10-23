fad171e19ae43475da11bf62423b163a59e42761
Maharoz <mahruj66@gmail.com>
2018-10-24 00:02:07 +0600

    abstract classes implemented

---
+++ CSharpAbstractClassDemo/CSharpAbstractClassDemo/Circle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAbstractClassDemo
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+}
---
+++ CSharpAbstractClassDemo/CSharpAbstractClassDemo/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAbstractClassDemo
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public abstract void Draw();
+        public void Copy()
+        {
+            Console.WriteLine("Copied Shape into clipboard");
+        }
+        public void Select()
+        {
+            Console.WriteLine("Select the shape");
+        }
+        
+    }
+}
