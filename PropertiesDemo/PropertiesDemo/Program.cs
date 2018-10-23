2573d6ac3568d6edfea98cdfe9983d4bdc28993d
Maharoz <mahruj66@gmail.com>
2018-10-23 21:36:09 +0600

    properties demonstrated

---
+++ PropertiesDemo/PropertiesDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
