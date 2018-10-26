68f941b90422906891f5852637d7c0cbe83c4751
Maharoz <mahruj66@gmail.com>
2018-10-26 12:40:46 +0600

    composition implemented

---
+++ CSharpCompositionDemo/CSharpCompositionDemo/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompositionDemo
+{
+    public class DbMigrator
+    {
+        private readonly Logger _logger;
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("We are migrating");
+        }
+    }
+}
---
+++ CSharpCompositionDemo/CSharpCompositionDemo/Installer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCompositionDemo
+{
+    public class Installer
+    {
+        private readonly Logger _logger;
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application");
+        }
+    }
+}
