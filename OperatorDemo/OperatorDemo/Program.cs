52cda41a02d82981d2a2237adb9da9b8bacb46b7
Maharoz <mahruj66@gmail.com>
2019-01-15 21:47:11 +0600

    operator demonstrated

---
+++ OperatorDemo/OperatorDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            var c = 3;
+            Console.WriteLine(a+b);
+            Console.WriteLine(a+b*c); //multiplication will work first
+        }
+    }
+}
