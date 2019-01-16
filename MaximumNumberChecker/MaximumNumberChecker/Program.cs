42451743ec734bae77df44c499d6276c575d3d8d
Maharoz <mahruj66@gmail.com>
2019-01-16 23:13:07 +0600

    maximum number finding

---
+++ MaximumNumberChecker/MaximumNumberChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumNumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two number to find maximum");
+            var a = Console.ReadLine();
+            var number1 = Convert.ToInt32(a);
+            var b = Console.ReadLine();
+            var number2 = Convert.ToInt32(b);
+            //if(number1 > number2)
+            //{
+            //    Console.WriteLine(string.Format("{0} is maximum",number1));
+            //}
+            //else
+            //{
+            //    Console.WriteLine(string.Format("{0} is maximum", number2));
+            //}
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("max number is " + max);
+
+        }
+    }
+}
