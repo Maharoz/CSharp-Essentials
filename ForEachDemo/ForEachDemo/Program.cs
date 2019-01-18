8f41e936d3fd97510355c7f23c1881d81fca148e
Maharoz <mahruj66@gmail.com>
2019-01-18 12:19:02 +0600

    foreach loop demo

---
+++ ForEachDemo/ForEachDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var name = "Maharoz Alam";
+            //foreach(var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
