c1b3eb70c2a6f2c8fccd127a787d77182a885578
Maharoz <mahruj66@gmail.com>
2019-01-19 21:19:36 +0600

    timespan demo

---
+++ TimeSpanDemo/TimeSpanDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating 
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration " + duration);
+
+            //properties
+            Console.WriteLine("Minutes:" + timeSpan.Minutes );
+            Console.WriteLine("Total Minutes:" + timeSpan.TotalMinutes);
+
+            
+
+        }
+    }
+}
