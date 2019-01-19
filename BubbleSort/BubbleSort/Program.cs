8d9c8bb3d490b528cc72414b3c823c6e62ba0d79
Maharoz <mahruj66@gmail.com>
2019-01-19 22:24:01 +0600

    bubble sort implemented

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Problem definition --> Bubble sort
+            int[] a = { 657, 900, 512, 269, 606 };
+            int t;
+            Console.WriteLine("The Array is : ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            
+            for (int j = 0; j <= a.Length - 2; j++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                    }
+                }
+            }
+            Console.WriteLine("The Sorted Array :");
+            foreach (int aray in a)
+                Console.Write(aray + " ");
+        }
+    }
+}
