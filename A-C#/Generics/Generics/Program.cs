c0b0b6251b9d44dd1acacd52a8e9ba982401d86d
maharoz <mahruj66@gmail.com>
2019-02-28 20:50:45 +0600

    Generics added

---
+++ A-C#/Generics/Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Equal = Calculator.AreEqual<string>("AB", "AB");
+
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public static bool AreEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
+        }
+    }
+}
