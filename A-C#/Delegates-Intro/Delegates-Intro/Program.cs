7bb706aec7842b510d025fbdff7fd3c919c444c2
maharoz <mahruj66@gmail.com>
2019-02-28 12:56:44 +0600

    introduction to deletegates

---
+++ A-C#/Delegates-Intro/Delegates-Intro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Intro
+{
+    public delegate void HellFunctionoDelegate(string strMessage);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A delegate is type safe function pointer
+            HellFunctionoDelegate del = new HellFunctionoDelegate(Hello);
+            del("Hello from delegate");
+        }
+
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
