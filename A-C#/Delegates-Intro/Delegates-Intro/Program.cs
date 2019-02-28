047df81cc976a57fc0c60d5d05fe88c9ded182ea
maharoz <mahruj66@gmail.com>
2019-02-28 14:11:09 +0600

    introduction to deletegates (update)

--- A-C#/Delegates-Intro/Delegates-Intro/Program.cs
+++ A-C#/Delegates-Intro/Delegates-Intro/Program.cs
@@ -6,19 +6,87 @@ using System.Threading.Tasks;
 
 namespace Delegates_Intro
 {
-    public delegate void HellFunctionoDelegate(string strMessage);
+  
     class Program
     {
         static void Main(string[] args)
         {
-            //A delegate is type safe function pointer
-            HellFunctionoDelegate del = new HellFunctionoDelegate(Hello);
-            del("Hello from delegate");
+           List<Employee> empList = new List<Employee>();
+
+            empList.Add(new Employee()
+            {
+                ID = 100,
+                Name = "Maharoz",
+                Salary = 5000,
+                Experience = 5
+            });
+
+            empList.Add(new Employee()
+            {
+                ID = 100,
+                Name = "Marry",
+                Salary = 5000,
+                Experience = 5
+            });
+
+            empList.Add(new Employee()
+            {
+                ID = 100,
+                Name = "Mike",
+                Salary = 4000,
+                Experience = 8
+            });
+
+            empList.Add(new Employee()
+            {
+                ID = 100,
+                Name = "Andrew",
+                Salary = 6000,
+                Experience = 4
+            });
+
+            empList.Add(new Employee()
+            {
+                ID = 100,
+                Name = "John",
+                Salary = 8000,
+                Experience = 3
+            });
+            IsPromotable isPromotable = new IsPromotable(Promote);
+
+            Employee.PromoteEmployee(empList,isPromotable);
         }
 
-        public static void Hello(string strMessage)
+        public static bool Promote(Employee emp)
         {
-            Console.WriteLine(strMessage);
+            if (emp.Experience > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+    public delegate bool IsPromotable(Employee empl);
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<Employee> employeeList, IsPromotable IsEligibleToPromote)
+        {
+            foreach (Employee employee in employeeList)
+            {
+                if (IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " promoted");
+                }
+            }
         }
     }
 }
