1d0f0c8fdcf49fb91060a27622401c31e4403820
maharoz <mahruj66@gmail.com>
2019-03-01 20:32:11 +0600

    Enum added

---
+++ A-C#/Enum/Enum/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer[] customer = new Customer[3];
+
+            customer[0] = new Customer
+            {
+                Name = "Maharoz",
+                Gender = Gender.Male
+
+            };
+            customer[1] = new Customer
+            {
+                Name = "Maharoz Mugdho",
+                Gender = Gender.Female
+
+            };
+            customer[2] = new Customer
+            {
+                Name = "Maharoz Alam",
+                Gender = Gender.Unknown
+
+            };
+        }
+
+        public static string GetGender(Gender gender)
+        {
+            switch(gender)
+            {
+                case Gender.Male:
+                    return "Male";
+                case Gender.Female:
+                    return "Female";
+                case Gender.Unknown:
+                    return "Unknown";
+                default:
+                    return "Invalid Data";
+            }
+           
+
+        }
+    }
+
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
---
+++ A-C#/Interface/Interface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface ICustomer
+    {
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
