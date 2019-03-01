9f93b79329f0beb2061361ef97d84977c2a7bc73
maharoz <mahruj66@gmail.com>
2019-03-01 23:10:53 +0600

    Interface added

--- A-C#/Interface/Interface/Program.cs
+++ A-C#/Interface/Interface/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Interface
 {
-    public interface ICustomer
+    public interface I1
     {
-
+        void InterfaceMethod();
     }
 
-    public class Program
+    public interface I2 {
+        void InterfaceMethod();
+    }
+
+    public class Program:I1,I2
     {
+        public void InterfaceMethod()
+        {
+            Console.WriteLine("T1 Interface method");
+        }
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.InterfaceMethod();
         }
     }
 }
---
+++ A-C#/Properties/Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+        public class Student
+        {
+            public int ID;
+            public int Name;
+            public int PassMark = 35;
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student c1 = new Student();
+        }
+    }
+}
