243d5b2417e17cf4122a24edd762af23a41f70de
maharoz <mahruj66@gmail.com>
2019-03-01 23:34:52 +0600

    properties added

--- A-C#/Properties/Properties/Program.cs
+++ A-C#/Properties/Properties/Program.cs
@@ -8,15 +8,54 @@ namespace Properties
 {
         public class Student
         {
-            public int ID;
-            public int Name;
-            public int PassMark = 35;
+            private int _id;
+            private string _Name;
+            private int _PassMark = 35;
+
+            public void setName(string Name)
+            {
+                if (string.IsNullOrEmpty(Name))
+                {
+                    throw new Exception("Name cannot be null or empty");
+            }
+            }
+
+            public string getName()
+            {
+                if (string.IsNullOrEmpty(this._Name))
+                {
+                    return "No name";
+                }
+                else
+                {
+                    return this._Name;
+                }
+            }
+
+            public void setId(int Id)
+            {
+                if (_id < 0)
+                {
+                    throw new Exception("Student id cannot be negative");
+                }
+
+                this._id = Id;
+            }
+
+            public int getId()
+            {
+                return this._id;
+            }
         }
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Student c1 = new Student();
+            c1.setId(101);
+            //c1.setName(null);
+            Console.WriteLine("Student id is {0}",c1.getId());
+            Console.WriteLine("Student name is {0}", c1.getName());
         }
     }
 }
