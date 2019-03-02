c3d27f7d3c6bd16d48c63b1b5d1ab2337e7535b0
maharoz <mahruj66@gmail.com>
2019-03-02 19:38:09 +0600

    abstract added

---
+++ A-C#/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Customer
+    {
+        public abstract void Print();
+
+    }
+    public class Program:Customer
+    {
+        public override void Print()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Print();
+        }
+    }
+}
