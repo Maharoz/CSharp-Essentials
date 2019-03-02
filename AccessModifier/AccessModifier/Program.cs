7db1cd3243867326b021e20e398c4ac5411306cb
maharoz <mahruj66@gmail.com>
2019-03-02 13:46:27 +0600

    access modifier

---
+++ AccessModifier/AccessModifier/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    public class Customer
+    {
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            //Console.WriteLine(c1._id);
+        }
+    }
+}
