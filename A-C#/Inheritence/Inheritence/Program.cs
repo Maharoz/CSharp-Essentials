25628dd7ff138155267b8371192b13b09f5224d6
maharoz <mahruj66@gmail.com>
2019-03-08 08:22:37 +0600

    inheritance implemented

---
+++ A-C#/Inheritence/Inheritence/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+
+
+ public class Employee
+ {
+     public string FirstName;
+     public string LastName;
+     public string Email;
+
+     public void PrintFullName()
+     {
+         Console.WriteLine(FirstName + " " + LastName);
+     }
+
+ }
+    //here FullTimeEmployee is derived class
+    // and Employee Parent class
+    public class FullTimeEmployee : Employee
+ {
+     public float YearlySalary;
+ }
+
+    //here PartTimeEmployee is derived class
+    // and Employee Parent class
+    public class PartTimeEmployee : Employee
+ {
+     public float HourlyRate;
+ }
+
+    //multilevel inheritence
+    public class A : PartTimeEmployee
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee FTE =new FullTimeEmployee();
+            FTE.FirstName = "Maharoz";
+            FTE.LastName = "ALAM";
+            FTE.YearlySalary = 500000;
+            FTE.PrintFullName();
+
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.FirstName = "ABC";
+            PTE.LastName = "SSS";
+            PTE.HourlyRate = 1000;
+            PTE.PrintFullName();
+
+            A A1 = new A();
+            A1.FirstName = "xyz";
+            A1.LastName = "abc";
+            A1.PrintFullName();
+        }
+    }
+}
