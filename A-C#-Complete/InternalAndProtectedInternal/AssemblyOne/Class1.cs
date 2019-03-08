419b96adb1eaca5f6dc764106193c9f6b5b58fbf
maharoz <mahruj66@gmail.com>
2019-03-08 12:13:14 +0600

    internal access modifier implemented

---
+++ A-C#-Complete/InternalAndProtectedInternal/AssemblyOne/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClassI
+    {
+        internal int ID = 101;
+    }
+
+    public class AssemblyOneClassII
+    {
+        public void SampleMethod()
+        {
+            AssemblyOneClassI A1 = new AssemblyOneClassI();
+            Console.WriteLine(A1.ID);
+        }
+    }
+}
---
+++ A-C#-Complete/InternalAndProtectedInternal/AssemblyTwo/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    public class AssemblyTwoClassI
+    {
+        public void Print()
+        {
+            AssemblyOneClassI A1 = new AssemblyOneClassI();
+            //BELOW LINE IS ILLEGAL BECAUSE ID 
+            //IS INTERNAL PROPERTY OF PROJECT AssemblyOne
+            //A1.ID = 101;
+        }
+    }
+}
