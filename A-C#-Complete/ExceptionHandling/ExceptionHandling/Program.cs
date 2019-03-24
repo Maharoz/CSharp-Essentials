f5789f49b86fe7a89e5694d164c1ba4c149895f2
maharoz <mahruj66@gmail.com>
2019-03-24 21:39:03 +0600

    exception handling basic

---
+++ A-C#-Complete/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(@"G:\Udemy\Data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the {0} file exists",ex.FileName);
+                
+            }
+            
+        }
+    }
+}
