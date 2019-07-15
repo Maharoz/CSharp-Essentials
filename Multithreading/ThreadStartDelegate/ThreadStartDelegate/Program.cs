bed7839089b0bfdc015a8f9e01d3dca763836808
maharoz <mahruj66@gmail.com>
2019-07-15 21:11:22 +0600

    Thread start deletegate

---
+++ Multithreading/ThreadStartDelegate/ThreadStartDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStartDelegate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number number = new Number();
+            Thread T1 = new Thread(new ThreadStart(number.PrintNumber));
+            T1.Start();
+        }
+    }
+
+    class Number
+    {
+        public void PrintNumber()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
