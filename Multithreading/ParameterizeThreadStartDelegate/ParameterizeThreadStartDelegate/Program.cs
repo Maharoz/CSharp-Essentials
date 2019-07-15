aa635c1dae047e1b04712f27bd2d848b2e2c1cc3
maharoz <mahruj66@gmail.com>
2019-07-15 21:24:25 +0600

    Parameterized Thread start

---
+++ Multithreading/ParameterizeThreadStartDelegate/ParameterizeThreadStartDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParameterizeThreadStartDelegate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the target number");
+           object target = Console.ReadLine();
+            Number number = new Number();
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(number.PrintNumber);
+            Thread T1 = new Thread(parameterizedThreadStart);
+            T1.Start(target);
+        }
+    }
+
+    class Number
+    {
+        public void PrintNumber(object target)
+        {
+            int number = 0;
+            if(int.TryParse(target.ToString(), out number))
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
