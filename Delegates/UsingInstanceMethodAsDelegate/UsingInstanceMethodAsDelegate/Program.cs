19bc02c9491f75ede47a5a6f60541c2de56f0929
Maharoz <mahruj66@gmail.com>
2021-01-10 19:40:57 +0600

    using instance method as delegates

---
+++ Delegates/UsingInstanceMethodAsDelegate/UsingInstanceMethodAsDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UsingInstanceMethodAsDelegate
+{
+    class Program
+    {
+        delegate string StrMod(string str);
+
+        
+        static void Main(string[] args)
+        {
+            StringOp so = new StringOp();
+            StrMod strOp = so.ReplaceSpaces;
+            string str;
+
+            str = strOp("This is test");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+
+            strOp = so.Removespace;
+            str = strOp("This is test");
+            Console.WriteLine("Resulting string: " + str);
+            Console.WriteLine();
+
+            strOp = so.reverse;
+            str = strOp("This is test");
+            Console.WriteLine("Resulting string: " + str);
+        }
+    }
+}
---
+++ Delegates/UsingInstanceMethodAsDelegate/UsingInstanceMethodAsDelegate/StringOp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingInstanceMethodAsDelegate
+{
+    public class StringOp
+    {
+        //Replace space With hyphen
+        public string ReplaceSpaces(string s)
+        {
+            Console.WriteLine("Replacing space with hyphen.");
+            return s.Replace(' ', '-');
+        }
+
+        //Remove space
+        public string Removespace(string s)
+        {
+            string temp = "";
+            int i;
+            Console.WriteLine("Removing space");
+            for (i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                    temp += s[i];
+            }
+
+            return temp;
+        }
+
+        //Reverse a string
+        public string reverse(string s)
+        {
+            string temp = "";
+            int i, j;
+            Console.WriteLine("Reversing string");
+            for (i = s.Length - 1; i >= 0; i--)
+                temp += s[i];
+            return temp;
+        }
+    }
+}
