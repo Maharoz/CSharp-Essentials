c8c2e3f16bb3596c7a75a8c5c07baaec07a7cad6
Maharoz <mahruj66@gmail.com>
2021-01-13 21:26:07 +0600

    Anonymous function/method added with outer variable nad returning value

---
+++ AnonymousFunction/OuterVariableWithAnonymousFunction/OuterVariableWithAnonymousFunction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OuterVariableWithAnonymousFunction
+{
+    delegate int CountIt(int end);
+    class Program
+    {
+        static CountIt Counter()
+        {
+            int sum = 0;
+
+            CountIt ctObj = delegate (int end)
+            {
+                for (int i = 0; i <= end; i++)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+                return sum;
+            };
+            return ctObj;
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            CountIt count = Counter();
+
+            int result;
+
+            result = count(3);
+            Console.WriteLine("Summation of 3 is:" + result);
+            Console.WriteLine();
+
+            result = count(5);
+            Console.WriteLine("Summation of 5 is:" + result);
+        }
+    }
+}
