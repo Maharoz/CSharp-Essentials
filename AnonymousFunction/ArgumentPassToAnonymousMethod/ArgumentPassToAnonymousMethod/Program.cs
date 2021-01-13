4a0518ae952aa6caeb580a74ceed45ac5445c045
Maharoz <mahruj66@gmail.com>
2021-01-13 21:11:04 +0600

    Anonymous function/method added with argument passing

---
+++ AnonymousFunction/ArgumentPassToAnonymousMethod/ArgumentPassToAnonymousMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArgumentPassToAnonymousMethod
+{
+    delegate void CountIt(int end);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            CountIt count = delegate (int end)
+            {
+                for (int i = 0; i < end; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            };
+
+
+            count(3);
+            Console.WriteLine();
+            count(5);
+        }
+    }
+}
