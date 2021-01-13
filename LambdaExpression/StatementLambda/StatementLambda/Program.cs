6f92bad0b541e956faa29ba1c27467f78306168b
Maharoz <mahruj66@gmail.com>
2021-01-13 22:05:42 +0600

    statement lambda implemented

---
+++ LambdaExpression/StatementLambda/StatementLambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StatementLambda
+{
+    class Program
+    {
+        delegate int IntOp(int end);
+        static void Main(string[] args)
+        {
+            IntOp fact = n =>
+            {
+                int r = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    r = i * r;
+                }
+                return r;
+            };
+            Console.WriteLine("The factorial of 3 is" + fact(3));
+            Console.WriteLine("The factorial of 3 is" + fact(5));
+        }
+    }
+}
