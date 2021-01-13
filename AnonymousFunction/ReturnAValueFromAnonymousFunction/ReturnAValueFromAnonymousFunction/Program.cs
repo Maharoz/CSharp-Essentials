0cf720ea386b6dc84b0a9ca9f120d10b3d259c18
Maharoz <mahruj66@gmail.com>
2021-01-13 21:19:22 +0600

    Anonymous function/method added with argument passing nad returning value

---
+++ AnonymousFunction/ReturnAValueFromAnonymousFunction/ReturnAValueFromAnonymousFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReturnAValueFromAnonymousFunction
+{
+    class Program
+    {
+        delegate int CountIt(int end);
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int result;
+
+            CountIt count = delegate (int end)
+            {
+                int sum = 0;
+
+                for(int i = 0; i<=end; i++)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+                return sum;
+            };
+
+            result = count(3);
+            Console.WriteLine("Summation of 3 is:" + result);
+            Console.WriteLine();
+
+            result = count(5);
+            Console.WriteLine("Summation of 5 is:" + result);
+        }
+    }
+}
