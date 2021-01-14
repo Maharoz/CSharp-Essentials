8953779d7a84f87a724b0668aa1919cc6cf5e8ec
Maharoz <mahruj66@gmail.com>
2021-01-14 21:00:48 +0600

    abstract class implemented

---
+++ AbstractClasses/AbstractClassDemo/AbstractClassDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.VIN = "25632";
+            Console.ReadLine();
+        }
+    }
+
+   
+
+    public abstract class Vehicle 
+    {
+        public string VIN {get; set; }
+        public string Manufracturer { get;set; }
+        public string Model { get; set; }
+        public int YearManufactured { get; set; }
+
+    }
+
+    public class Car : Vehicle
+    {
+        public int NOofWheel { get; set; } = 4;
+    }
+}
