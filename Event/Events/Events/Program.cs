fa7990d643b03108b96bc42dec6852c06d1e1d4c
Maharoz <mahruj66@gmail.com>
2021-01-16 21:09:40 +0600

    event implemented

---
+++ Event/Events/Events/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        delegate void MyEventHandler();
+        class MyEvent
+        {
+            public event MyEventHandler SomeEvent;
+            public void OneSomeEvent()
+            {
+                if (SomeEvent != null)
+                    SomeEvent();
+            }
+        }
+        static void Handler()
+        {
+            Console.WriteLine("Event occured");
+        }
+        static void Main(string[] args)
+        {
+            MyEvent evt = new MyEvent();
+            evt.SomeEvent += Handler;
+            evt.OneSomeEvent();
+        }
+    }
+}
