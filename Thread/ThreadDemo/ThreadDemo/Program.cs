d2f1513100997bdc4d0fcb58a2b6c1c1a08dec6c
Maharoz <mahruj66@gmail.com>
2021-01-19 20:56:34 +0600

    simple thread implemented

---
+++ Thread/ThreadDemo/ThreadDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo
+{
+
+    public class MyThread
+    {
+        public int Count;
+        string thrdName;
+
+        public MyThread(string name){
+            Count = 0;
+            thrdName = name;
+     }
+
+        public void run()
+        {
+            Console.WriteLine(thrdName + " starting.");
+
+            do
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In " + thrdName + ", Count is " + Count);
+                Count++;
+            } while (Count < 10);
+
+            Console.WriteLine(thrdName + " terminating.");
+        }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+            MyThread mt = new MyThread("Child#1");
+
+            Thread newThrd = new Thread(mt.run);
+
+            newThrd.Start();
+
+            do
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(100);
+            } while (mt.Count != 10);
+
+            Console.WriteLine("Main thread ending!");
+    }
+}
+}
