2abfdd23ba0dbaff7e499c40ca387d4a15e40818
Maharoz <mahruj66@gmail.com>
2021-01-21 21:28:40 +0600

    swap using ref implemented

---
+++ RefAndOut/SwapTwoValueUsingRef/SwapTwoValueUsingRef/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwapTwoValueUsingRef
+{
+    public class swap
+    {
+        public void swapRef(ref int a , ref int b)
+        {
+            int t;
+            t = a;
+            a = b;
+            b = t;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;int y = 20;
+            swap s = new swap();
+            Console.WriteLine("x and y before call " + x +" " + y);
+            s.swapRef(ref x, ref y);
+            Console.WriteLine("x and y after call " + x + " " + y);
+
+
+        }
+    }
+}
