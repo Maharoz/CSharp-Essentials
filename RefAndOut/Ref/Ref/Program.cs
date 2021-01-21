f7a250c7fa9a656ef537174606f05535e18591fe
Maharoz <mahruj66@gmail.com>
2021-01-21 21:21:18 +0600

    ref implemented

---
+++ RefAndOut/Ref/Ref/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ref
+{
+    public class RefTest
+    {
+        public void sqr(ref int i)
+        {
+            i= i * i;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RefTest r = new RefTest();
+            int a = 10;
+            Console.WriteLine("a before call: " +a);
+
+            r.sqr(ref a);
+
+            Console.WriteLine("a after call: " + a);
+        }
+    }
+}
