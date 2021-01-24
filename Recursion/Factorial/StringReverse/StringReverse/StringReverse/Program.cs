525ca499bd93e093c503b27c8ba9facd922f3ac5
Maharoz <mahruj66@gmail.com>
2021-01-24 21:10:57 +0600

    string reverse implemented

---
+++ Recursion/Factorial/StringReverse/StringReverse/StringReverse/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "This is test";
+            RevStr r = new RevStr();
+            Console.WriteLine($"Original string {s}");
+
+            r.DisplayRev(s);
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ Recursion/Factorial/StringReverse/StringReverse/StringReverse/RevStr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringReverse
+{
+    public class RevStr
+    {
+        public void DisplayRev(string str)
+        {
+            if (str.Length > 0)
+            {
+                DisplayRev(str.Substring(1, str.Length - 1));
+            }
+            else { return; }
+            Console.Write(str[0]);
+           // Console.Write(str);
+        }
+    }
+}
