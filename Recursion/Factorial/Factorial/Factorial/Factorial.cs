e5fc34dffded7ffcfc73cb50280563275bb23681
Maharoz <mahruj66@gmail.com>
2021-01-24 20:52:39 +0600

    factorial implemented

---
+++ Recursion/Factorial/Factorial/Factorial/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial
+{
+    public class Factorial
+    {
+        public int FactR(int n)
+        {
+            int result;
+
+            if (n == 1) return 1;
+
+            result = FactR(n - 1) * n;
+            return result;
+        }
+
+        public int FactI(int n)
+        {
+            int t, result;
+
+            result = 1;
+            for (t = 1; t <= n; t++)
+            {
+                result *= t;
+               
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Recursion/Factorial/Factorial/Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial();
+            Console.WriteLine("Factorial of 3 is " + f.FactR(3));
+        }
+    }
+}
