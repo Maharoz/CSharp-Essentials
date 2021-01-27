468a972356c08afec9f007244c765955133587ed
Maharoz <mahruj66@gmail.com>
2021-01-27 23:10:59 +0600

    pausing threading implemented

---
+++ Multithreading/PausingAThread/PausingAThread/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace PausingAThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintNumbers()
+            {
+                for(int i = 1; i< 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            static void PrintNumberWithDelay()
+            {
+                Console.WriteLine("Starting...");
+                for (int i = 1; i < 10; i++)
+                {
+                    Sleep(TimeSpan.FromSeconds(2));
+                    Console.WriteLine(i);
+                }
+            }
+
+            Thread t = new Thread(PrintNumberWithDelay);
+            t.Start();
+            PrintNumbers();
+        }
+    }
+}
