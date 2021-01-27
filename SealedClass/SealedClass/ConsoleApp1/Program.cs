572a2bbd6634dfe5f87a3fc7c9a36084a08540f8
Maharoz <mahruj66@gmail.com>
2021-01-27 22:56:22 +0600

    sealed class implemented

---
+++ SealedClass/SealedClass/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    sealed class SealedClass
+    {
+        public int Add(int a ,int b)
+        {
+            return a + b;   
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SealedClass slc = new SealedClass();
+
+            int total = slc.Add(6, 4);
+            Console.WriteLine("Total = " + total.ToString());
+        }
+    }
+}
