78442bcc0981f8a94b123ef5cdee3cc5b52d046d
Maharoz <mahruj66@gmail.com>
2021-01-27 23:28:55 +0600

    use of thread abort

---
+++ Multithreading/AbortingThread/AbortingThread/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace AbortingThread
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			
+				Console.WriteLine("Starting program...");
+				Thread t = new Thread(PrintNumbersWithDelay);
+				t.Start();
+				Thread.Sleep(TimeSpan.FromSeconds(6));
+				t.Abort();
+				Console.WriteLine("A thread has been aborted");
+			
+
+			static void PrintNumbersWithDelay()
+			{
+				Console.WriteLine("Starting...");
+				for (int i = 1; i < 10; i++)
+				{
+					Thread.Sleep(TimeSpan.FromSeconds(2));
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
