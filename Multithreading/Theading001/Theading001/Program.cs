7f8cd911db1e33e8cd39e051b218b255078f46b6
Maharoz <mahruj66@gmail.com>
2021-01-27 23:05:17 +0600

    simple threading implemented

---
+++ Multithreading/Theading001/Theading001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Theading001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintNumbers()
+            {
+                Console.WriteLine("Starting...");
+                for (int i = 1; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintNumbers();
+        }
+    }
+
+    
+}
