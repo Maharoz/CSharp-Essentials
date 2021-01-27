056b2fdce394d81c405e61ef2f4b33cdcc4ce513
Maharoz <mahruj66@gmail.com>
2021-01-27 23:18:54 +0600

    use of thread join

---
+++ Multithreading/MakingThreadWait/MakingThreadWait/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace MakingThreadWait
+{
+    class Program
+    {
+
+        static void PrintNumbersWithDelay()
+        {
+            Console.WriteLine("Starting...");
+            for(int i = 1; i < 10; i++)
+            {
+
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Thread t = new Thread(PrintNumbersWithDelay);
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thead completed");
+
+        }
+    }
+}
