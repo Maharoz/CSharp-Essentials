f07adfb78c59daf2eb6246950ab56630c7624a8a
Maharoz <mahruj66@gmail.com>
2022-10-17 19:41:37 +0600

    Thread class constructor

---
+++ Thread/ThreadClassCreation/ThreadClassCreation/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Example to Understand Constructors of Thread Class in C#.
+
+//Thread t1 = new Thread(DisplayNumber);
+//t1.Start();
+//Console.Read();
+
+//static void DisplayNumber()
+//{
+//    for(int i = 0; i <= 5; i++)
+//    {
+//        Console.WriteLine("Method1 :" +i);
+//    }
+//}
+
+//Example to Understand ThreadStart Delegate in C#
+
+//ThreadStart obj = new ThreadStart(DisplayNumbers);
+
+//Thread t1 = new Thread(obj);
+//t1.Start();
+//Console.Read();
+
+//static void DisplayNumbers()
+//{
+//    for (int i = 1; i <= 5; i++)
+//    {
+//        Console.WriteLine("Method1 :" + i);
+//    }
+//}
+
+//Creating Thread Class Instance using Anonymous Method in C#
+
+Thread t1 = new Thread(delegate ()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Method1: "+i);
+    }
+});
+
+t1.Start();
+Console.Read();
