63aaa76e4883706f10a8a185c6d292aa2782b4e6
Maharoz <mahruj66@gmail.com>
2022-10-17 19:48:24 +0600

    Parametrized thread

---
+++ Thread/ParameterizedThreadConstructor/ParameterizedThreadConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Program obj = new Program();
+        ParameterizedThreadStart PTSD = new ParameterizedThreadStart(obj.DisplayNumbers);
+        Thread t1 = new Thread(PTSD);
+        t1.Start(5);
+        Console.Read();
+    }
+
+
+    void DisplayNumbers(object Max)
+    {
+        int Number = Convert.ToInt32(Max);
+        for (int i = 1; i <= Number; i++)
+        {
+            Console.WriteLine("Method1 :" + i);
+        }
+    }
+}
+
