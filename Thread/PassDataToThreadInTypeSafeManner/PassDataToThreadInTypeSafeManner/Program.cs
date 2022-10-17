6ee5f9ae037e232080a350944a35966094cb9a60
Maharoz <mahruj66@gmail.com>
2022-10-17 20:04:17 +0600

    how to pass data to a thread in type safe manner

---
+++ Thread/PassDataToThreadInTypeSafeManner/PassDataToThreadInTypeSafeManner/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max = 10;
+
+NumberHelper obj = new NumberHelper(Max);
+Thread t1 = new Thread(new ThreadStart(obj.DisplayNumber));
+t1.Start();
+Console.Read();
+public class NumberHelper
+{
+    int _Number;
+    public NumberHelper(int Number)
+    {
+        _Number = Number;
+    }
+
+    public void DisplayNumber()
+    {
+        for (int i = 0; i < _Number; i++)
+        {
+            Console.WriteLine("value : " +i);
+        }
+    }
+}
+
+
