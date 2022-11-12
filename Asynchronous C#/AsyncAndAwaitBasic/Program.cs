feca30cf3e5395b4551f89ac806b820a6fd2a103
Maharoz <mahruj66@gmail.com>
2022-11-12 15:22:33 +0600

    Asynchronous programming added

---
+++ Asynchronous C#/AsyncAndAwaitBasic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Main Method stated");
+SomeMethod();
+Console.WriteLine("Main Method end");
+Console.ReadKey();
+
+
+async static void SomeMethod()
+{
+    Console.WriteLine("Some Method started");
+
+    await Task.Delay(TimeSpan.FromSeconds(5));
+    Console.WriteLine("\n");
+    Console.WriteLine("Some method ends");
+}
