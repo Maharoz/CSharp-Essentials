ccf7f66c044c2a24b078af396005e0a14492853b
Maharoz <mahruj66@gmail.com>
2022-11-25 23:46:09 +0600

    c sharp init

---
+++ Init/InitInCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InitInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                FirstName = "Maharoz",
+                LastName="Mugdho"
+            };
+            Console.WriteLine(person.FirstName+ " " + person.LastName);
+            person.LastName = "Alam";
+            Console.WriteLine(person.FirstName + " " + person.LastName);
+        }
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+   
+}
